b80f3b80fe4e09b682f1aff8094e4f638e37c7ba
rahul <digitalguidepatna2@gmail.com>
2022-11-26 11:11:37 +0530

    home page and product page addedd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap'
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <Container className='py-3'>     
+            <Routes>
+              <Route path='/' element={<HomeScreen/>} />
+              <Route path='/product/:id' element={<ProductScreen/>} />
+            </Routes>   
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/product.js
@@ -0,0 +1,74 @@
+const product = [
+    {
+        '_id':'1',
+        'name':"this is freeeze name",
+        'image':'/images/freeze.png',
+        'description':
+            'dis for product',
+        'brand':'Apple',
+        'category': 'Electronic',
+        'price':100,
+        'countInStock':0,
+        'rating':1,
+        'numReviews':12,
+
+    },
+    {
+        '_id':'2',
+        'name':"this is iron name",
+        'image':'/images/iron.jpg',
+        'description':
+            'dis for product',
+        'brand':'Apple',
+        'category': 'Electronic',
+        'price':500,
+        'countInStock':10,
+        'rating':2.5,
+        'numReviews':12,
+
+    },
+    {
+        '_id':'3',
+        'name':"this is washing machine name",
+        'image':'/images/washing.jpg',
+        'description':
+            'dis for product',
+        'brand':'Apple',
+        'category': 'Electronic',
+        'price':100,
+        'countInStock':10,
+        'rating':4.5,
+        'numReviews':12,
+
+    },
+    {
+        '_id':'4',
+        'name':"this is fan name",
+        'image':'/images/fan.jpg',
+        'description':
+            'dis for product',
+        'brand':'Apple',
+        'category': 'Electronic',
+        'price':100,
+        'countInStock':10,
+        'rating':4.5,
+        'numReviews':12,
+
+    },
+    {
+        '_id':'5',
+        'name':"this is wahing name",
+        'image':'/images/washing.jpg',
+        'description':
+            'dis for product',
+        'brand':'Apple',
+        'category': 'Electronic',
+        'price':100,
+        'countInStock':10,
+        'rating':4.5,
+        'numReviews':12,
+
+    },
+]
+
+export default product;
